627848352c40fd4bd4a49602a80311e40bf5c280
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-07-22 20:22:11 +0600

    childrenProp

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import "./App.css";
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["Khulna", "Dhaka", "Rajshahi", "CTG", "Sylhet"];
+  let items = [
+    "Khulna",
+    "Dhaka",
+    "Rajshahi",
+    "CTG",
+    "Sylhet",
+    "Rongpur",
+    "Gazipur",
+    "Kumilla",
+  ];
 
   return (
     <>
       <div>
-        <ListGroup items={items} heading="Cities"/>
+        <Alert>
+          Hello <span>World</span>
+        </Alert>
+        <ListGroup items={items} heading="Cities" />
       </div>
     </>
   );
