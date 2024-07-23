8249493e7e6f1ba7616618af35b13adc0223ebe2
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-07-23 06:49:04 +0600

    alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = [
-    "Khulna",
-    "Dhaka",
-    "Rajshahi",
-    "CTG",
-    "Sylhet",
-    "Rongpur",
-    "Gazipur",
-    "Kumilla",
-  ];
-
+  const [alertVisibility, setAlertVisibility] = useState(false);
   return (
     <>
       <div>
-        <Alert>
-          Hello <span>World</span>
-        </Alert>
-        <Button color="primary" onClick={() => console.log("Clicked")}>
+        {alertVisibility && (
+          <Alert onClose={() => setAlertVisibility(false)}>
+            Hello <span>World</span>
+          </Alert>
+        )}
+        <Button color="primary" onClick={() => setAlertVisibility(true)}>
           My Button
         </Button>
-        <ListGroup items={items} heading="Cities" />
       </div>
     </>
   );
