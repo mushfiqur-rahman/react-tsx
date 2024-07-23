8c684cd7c5e71799c772787c4ccb51b7b8b00b1e
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-07-23 07:00:08 +0600

    style with vanila css

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,19 @@ import { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = [
+    "Khulna",
+    "Dhaka",
+    "Rajshahi",
+    "CTG",
+    "Sylhet",
+    "Rongpur",
+    "Gazipur",
+    "Kumilla",
+  ];
   const [alertVisibility, setAlertVisibility] = useState(false);
   return (
     <>
@@ -16,6 +27,7 @@ function App() {
         <Button color="primary" onClick={() => setAlertVisibility(true)}>
           My Button
         </Button>
+        <ListGroup items={items} heading="Cities" />
       </div>
     </>
   );
